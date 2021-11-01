ec2996bfeab14411b3dcfb0dbf4ade4875c855c5
Leonid Balikhin <balikhin.lb@gmail.com>
2021-11-01 22:12:35 +0300

    add gateway-server

--- eureka-server/src/main/java/lb/microservice/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/lb/microservice/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.lb.eurekaserver;
+package lb.microservice.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
