afce23d121353b8460ddb9f53641733307080328
Leonid Balikhin <balikhin.lb@gmail.com>
2021-11-07 06:47:22 +0300

    add security for product-composite-service

--- gateway/src/main/java/lb/microservice/gateway/SecurityConfig.java
+++ gateway/src/main/java/lb/microservice/gateway/SecurityConfig.java
@@ -12,14 +12,14 @@ public class SecurityConfig {
 		http.csrf()
 			.disable()
 			.authorizeExchange()
-			.pathMatchers("/headerrouting/**").permitAll()
-			.pathMatchers("/actuator/**").permitAll()
-			.pathMatchers("/eureka/**").permitAll()
+//			.pathMatchers("/headerrouting/**").permitAll()
+//			.pathMatchers("/actuator/**").permitAll()
+//			.pathMatchers("/eureka/**").permitAll()
 			.pathMatchers("/oauth2/**").permitAll()
 			.pathMatchers("/login/**").permitAll()
 			.pathMatchers("/error/**").permitAll()
-			.pathMatchers("/openapi/**").permitAll()
-			.pathMatchers("/webjars/**").permitAll()
+//			.pathMatchers("/openapi/**").permitAll()
+//			.pathMatchers("/webjars/**").permitAll()
 			.anyExchange().authenticated().and().oauth2ResourceServer().jwt();
 		return http.build();
 	}
